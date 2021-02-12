8fb53c937e72a49244f0ea960595133ee9d84e28
birolgunes <birol.gunes7@gmail.com>
2021-02-12 18:18:37 +0300

    add Ladder Script

---
+++ Ladder.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ladder : MonoBehaviour
+{
+    [HideInInspector] GameObject _Ladder;
+    [HideInInspector] public GameObject topTransform, botTransform;
+    [HideInInspector] public GameObject BotPlatform, TopPlatform;
+    [HideInInspector] public GameObject TopDrop, BotDrop;
+    void Awake()
+    {
+        _Ladder = transform.Find("Ladder").gameObject;
+
+        topTransform = transform.Find("TopTransform").gameObject;
+        botTransform = transform.Find("BotTransform").gameObject;
+
+        TopPlatform = transform.Find("TopPlatform").gameObject;
+        BotPlatform = transform.Find("BotPlatform").gameObject;
+
+        TopDrop = transform.Find("TopDrop").gameObject;
+        BotDrop = transform.Find("BotDrop").gameObject;
+    }
+}
